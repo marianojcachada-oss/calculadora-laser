5093220f28ebd50a04d44b7a2409afccb172d73e
marianojcachada-oss <marianojcachada@gmail.com>
2026-01-10 23:43:28 -0300

    Update tarifas.js

--- tarifas.js
+++ tarifas.js
@@ -14,23 +14,28 @@ function calcular() {
   let fieldname1 = fieldname2 * 2 + 2;
 
   if (fieldname1 < 6) {
-    out("sin_descuento").innerText = "$  6";
     fieldname1 = 6;
+    out("sin_descuento").innerText = "$ 6";
   } else {
-    if (fieldname21 == 1) fieldname1 += 2;
+    if (fieldname21 === 1) fieldname1 += 2;
     else if (fieldname21 > 1) fieldname1 += 2 + (fieldname21 - 1);
-    out("sin_descuento").innerText = "$  " + fieldname1.toFixed(2);
+
+    out("sin_descuento").innerText = "$ " + fieldname1.toFixed(2);
   }
 
   /* =========================
      CLASIFICACIÓN
   ========================= */
   let clasificacion = "";
-  if (fieldname1 < 19.75) clasificacion = "Es menor a $19,75";
-  else if (fieldname1 <= 29.75) clasificacion = "Es mayor a $19,75 pero menor a $29,75";
-  else if (fieldname1 < 40) clasificacion = "Es mayor a $29,75 pero menor a $40,00";
-  else clasificacion = "Es mayor a $40,00";
-
+  if (fieldname1 < 19.75) {
+    clasificacion = "Es menor a $19,75";
+  } else if (fieldname1 <= 29.75) {
+    clasificacion = "Es mayor a $19,75 pero menor a $29,75";
+  } else if (fieldname1 < 40) {
+    clasificacion = "Es mayor a $29,75 pero menor a $40,00";
+  } else {
+    clasificacion = "Es mayor a $40,00";
+  }
   out("clasificacion").innerText = clasificacion;
 
   /* =========================
@@ -47,84 +52,110 @@ function calcular() {
   /* =========================
      TARIFA CON DESCUENTO
   ========================= */
-  let discount = 0;
   let valor = fieldname2 * 2 + 2;
+  let discount = 0;
 
   if (valor < 7) {
-    out("con_descuento").innerText = "$  6";
+    out("con_descuento").innerText = "$ 6";
   } else {
     if (valor < 19.75) discount = valor * 0.9;
     else if (valor <= 29.75) discount = valor * 0.85;
     else if (valor < 40) discount = valor * 0.8;
     else discount = valor * 0.75;
 
-    if (fieldname21 == 1) discount += 2;
+    if (fieldname21 === 1) discount += 2;
     else if (fieldname21 > 1) discount += 2 + (fieldname21 - 1);
 
-    out("con_descuento").innerText = "$  " + discount.toFixed(2);
+    out("con_descuento").innerText = "$ " + discount.toFixed(2);
   }
 
   /* =========================
      VAN
   ========================= */
-  let van = valor < 8 ? "$  8" : Math.round(
-    (valor < 19.75 ? valor :
-     valor <= 39.75 ? valor * 0.9 :
-     valor * 0.85)
-    + (fieldname21 > 0 ? 2 + Math.max(0, fieldname21 - 1) : 0)
-  );
-  out("van").innerText = van;
+  let vanValue;
+  if (valor < 8) {
+    vanValue = 8;
+  } else {
+    if (valor < 19.75) vanValue = valor;
+    else if (valor <= 39.75) vanValue = valor * 0.9;
+    else vanValue = valor * 0.85;
+
+    if (fieldname21 === 1) vanValue += 2;
+    else if (fieldname21 > 1) vanValue += 2 + (fieldname21 - 1);
+  }
+  out("van").innerText = "$ " + Math.round(vanValue);
 
   /* =========================
      TRABAJO
   ========================= */
-  let trabajo = valor < 7 ? "$  6" : Math.round(
-    (valor < 19.75 ? valor * 0.8 : valor * 0.75)
-    + (fieldname21 > 0 ? 2 + Math.max(0, fieldname21 - 1) : 0)
-  );
-  out("trabajo").innerText = trabajo;
+  let trabajoValue;
+  if (valor < 7) {
+    trabajoValue = 6;
+  } else {
+    trabajoValue = valor < 19.75 ? valor * 0.8 : valor * 0.75;
+
+    if (fieldname21 === 1) trabajoValue += 2;
+    else if (fieldname21 > 1) trabajoValue += 2 + (fieldname21 - 1);
+  }
+  out("trabajo").innerText = "$ " + Math.round(trabajoValue);
 
   /* =========================
      TRABAJO VAN
   ========================= */
-  let trabajoVan = valor < 8 ? "$  8" : Math.round(
-    (valor < 19.75 ? valor * 0.9 : valor * 0.85)
-    + (fieldname21 > 0 ? 2 + Math.max(0, fieldname21 - 1) : 0)
-  );
-  out("trabajo_van").innerText = trabajoVan;
+  let trabajoVanValue;
+  if (valor < 8) {
+    trabajoVanValue = 8;
+  } else {
+    trabajoVanValue = valor < 19.75 ? valor * 0.9 : valor * 0.85;
+
+    if (fieldname21 === 1) trabajoVanValue += 2;
+    else if (fieldname21 > 1) trabajoVanValue += 2 + (fieldname21 - 1);
+  }
+  out("trabajo_van").innerText = "$ " + Math.round(trabajoVanValue);
 
   /* =========================
      DELIVERY COMIDA
   ========================= */
-  let comida = fieldname2 <= 2.6 ? 12 : (
-    valor < 19.75 ? valor * 0.9 + 5 :
-    valor < 29.75 ? valor * 0.85 + 5 :
-    valor * 0.8 + 5
-  );
-  comida += fieldname21 > 0 ? 2 + Math.max(0, fieldname21 - 1) : 0;
-  out("delivery_comida").innerText = Math.round(comida);
+  let comidaValue;
+  if (fieldname2 <= 2.6) {
+    comidaValue = 12;
+  } else {
+    if (valor < 19.75) comidaValue = valor * 0.9 + 5;
+    else if (valor < 29.75) comidaValue = valor * 0.85 + 5;
+    else comidaValue = valor * 0.8 + 5;
+  }
+
+  if (fieldname21 === 1) comidaValue += 2;
+  else if (fieldname21 > 1) comidaValue += 2 + (fieldname21 - 1);
+
+  out("delivery_comida").innerText = "$ " + Math.round(comidaValue);
 
   /* =========================
      DELIVERY ALCOHOL
   ========================= */
-  let alcohol = fieldname2 <= 4.8 ? 17 : (
-    valor < 19.75 ? valor * 0.9 + 7 :
-    valor < 29.75 ? valor * 0.85 + 7 :
-    valor * 0.8 + 7
-  );
-  alcohol += fieldname21 > 0 ? 2 + Math.max(0, fieldname21 - 1) : 0;
-  out("delivery_alcohol").innerText = Math.round(alcohol);
+  let alcoholValue;
+  if (fieldname2 <= 4.8) {
+    alcoholValue = 17;
+  } else {
+    if (valor < 19.75) alcoholValue = valor * 0.9 + 7;
+    else if (valor < 29.75) alcoholValue = valor * 0.85 + 7;
+    else alcoholValue = valor * 0.8 + 7;
+  }
+
+  if (fieldname21 === 1) alcoholValue += 2;
+  else if (fieldname21 > 1) alcoholValue += 2 + (fieldname21 - 1);
+
+  out("delivery_alcohol").innerText = "$ " + Math.round(alcoholValue);
 
   /* =========================
      OBJETO PERDIDO
   ========================= */
-  let obj = (fieldname2 * 2 + 2) * 0.75;
-  out("objeto").innerText = obj < 6 ? "$  6" : "$  " + Math.round(obj);
+  let objetoValue = (fieldname2 * 2 + 2) * 0.75;
+  if (objetoValue < 6) objetoValue = 6;
+
+  out("objeto").innerText = "$ " + Math.round(objetoValue);
 }
 
 millasInput.addEventListener("input", calcular);
 paradasInput.addEventListener("input", calcular);
 calcular();
-
-
-
